f98c88315ef1d57b45a660a1e597d81813718024
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-01 16:40:34 -0300

    Utils: Cria a DateUtils para trabalhar com datas

---
+++ app/Utils/DateUtils.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Utils;
+
+use Carbon\Carbon;
+
+date_default_timezone_set('America/Sao_Paulo');
+setlocale(LC_ALL, 'pt_BR.utf-8', 'ptb', 'pt_BR', 'portuguese-brazil', 'portuguese-brazilian', 'bra', 'brazil', 'br');
+setlocale(LC_TIME, 'pt_BR.utf-8', 'ptb', 'pt_BR', 'portuguese-brazil', 'portuguese-brazilian', 'bra', 'brazil', 'br');
+
+class DateUtils {
+
+    public const pt_BR = 'dddd, DD \\d\\e MMMM \\d\\e gggg \\a\\s HH:mm';
+
+    public static function getAsDate($date, $format = 'Y-m-d H:i:s') {
+        return Carbon::createFromFormat($format, date($format, strtotime($date)));
+    }
+
+}
