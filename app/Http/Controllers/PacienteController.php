2c7b2c3c144cf704dc6c399351a591746182b185
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-01 17:27:09 -0300

    Controller (Paciente): Criando métodos para gerenciar (+)
    - Cadastrar
    - Editar
    - Excluir

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Validators\Paciente\CadastrarValidator;
+use App\Validators\Paciente\EditarValidator;
+use App\Actions\Paciente\CadastrarAct;
+use App\Actions\Paciente\EditarAct;
+use App\Actions\Paciente\ExcluirAct;
+use App\Exceptions\ValidatorException;
+use Exception;
 
 class PacienteController extends Controller
 {
-    //
+
+    private $cadastrar, $editar, $excluir;
+
+    public function __construct(CadastrarAct $cadastrar, EditarAct $editar, ExcluirAct $excluir) {
+        $this->cadastrar = $cadastrar;
+        $this->editar = $editar;
+        $this->excluir = $excluir;
+    }
+    
+    public function cadastrar() {
+        try {
+            $dados = request()->all();
+
+            CadastrarValidator::validar($dados);
+
+            return response()->json(
+                $this->cadastrar->executar($dados),
+                Response::HTTP_OK
+            );
+        }
+        catch (ValidatorException $e) {
+            return response()->json($e->getErrors(), Response::HTTP_BAD_REQUEST);
+        }
+        catch (Exception $e) {
+            return response()->json(['mensagem' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function editar($id) {
+        try {
+            $dados = request()->all();
+
+            EditarValidator::validar($dados);
+
+            return response()->json(
+                $this->editar->executar($id, $dados),
+                Response::HTTP_OK
+            );
+        }
+        catch (ValidatorException $e) {
+            return response()->json($e->getErrors(), Response::HTTP_BAD_REQUEST);
+        }
+        catch (Exception $e) {
+            return response()->json(['mensagem' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function deletar($id) {
+        try {
+            return response()->json($this->excluir->executar($id), Response::HTTP_OK);
+        }
+        catch (Exception $e) {
+            return response()->json(['mensagem' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
