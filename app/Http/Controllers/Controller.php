916c2ccfa55939460e7474efd59bed414854d186
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-01 14:39:21 -0300

    Adiciona método medico na base controller (+)
    Apenas para simplificar quando precisar obter o médico logado no sistema nos controllers...

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Retorna o user.
+     *
+     * @return Medico
+     */
+    protected function medico() {
+        return Auth::user();
+    }
 }
