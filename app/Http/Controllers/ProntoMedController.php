0cce22222515f313992cc80122de6743f84c3239
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-01 14:41:56 -0300

    Controller: ProntoMed (responsável por exibir o front)

--- app/Http/Controllers/ProntoMedController.php
+++ app/Http/Controllers/ProntoMedController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Paciente;
 
-class ProntoMedController extends Controller
+class ProntoMedController extends SessionController
 {
-    //
+    public function index() {
+        // Verifica se o médico ainda possui acesso ao sistema.
+        if (parent::medico()->status == 'ativo') {
+
+            return view('paginas.principal', [
+                'tipos_sanguineo' => Paciente::opcoes['tipo_sanguineo'],
+                'sexos' => Paciente::opcoes['sexo']
+            ]);
+
+        } else {
+            return parent::logout();
+        }
+    }
 }
