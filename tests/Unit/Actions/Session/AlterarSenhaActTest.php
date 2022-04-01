15f60b26b605266c0bcc9e869c847913a0cd68ca
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-01 18:20:21 -0300

    Teste unitários para Session Acts (+)
    - Login
    - AlterarSenha

---
+++ tests/Unit/Actions/Session/AlterarSenhaActTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Actions\Session;
+
+use Tests\TestCase;
+use App\Actions\Session\AlterarSenhaAct;
+use App\Repositories\MedicoRepository;
+use App\Models\Medico;
+use Mockery;
+use Exception;
+
+class AlterarSenhaActTest extends TestCase {
+
+    private function getMockedSut() {
+        $medicoRepository = (object) Mockery::mock(MedicoRepository::class);
+        $act = new AlterarSenhaAct($medicoRepository);
+        return compact([ 'medicoRepository', 'act' ]);
+    }
+
+    /** @test */
+    public function deve_receber_exception_caso_a_senha_atual_nao_confere() {
+        // Arrange
+        $sut = $this->getMockedSut();
+        $usuario = Medico::factory(['senha' => 'admin000'])->make();
+        $dados = ['senha-atual' => '000admin', 'senha-nova' => '00admin00'];
+        
+        // Assert
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('A sua senha atual está incorreta.');
+
+        // Run
+        $sut['act']->executar($usuario, $dados);
+    }
+
+    /** @test */
+    public function deve_alterar_a_senha() {
+        // Arrange
+        $sut = $this->getMockedSut();
+        $usuario = Medico::factory(['senha' => 'admin000'])->make();
+        $dados = ['senha-atual' => 'admin000', 'senha-nova' => '00admin00'];
+
+        // Assert
+        $sut['medicoRepository']->shouldReceive('editar_medico')->once()->with($usuario, ['senha' => $dados['senha-nova']])->andReturn($usuario);
+
+        // Run
+        $sut['act']->executar($usuario, $dados);
+    }
+
+}
---
+++ tests/Unit/Actions/Session/LoginActTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Unit\Actions\Session;
+
+use Tests\TestCase;
+use App\Actions\Session\LoginAct;
+use App\Repositories\MedicoRepository;
+use App\Models\Medico;
+use Mockery;
+use Exception;
+
+class LoginActTest extends TestCase {
+
+    private function getMockedSut() {
+        $medicoRepository = (object) Mockery::mock(MedicoRepository::class);
+        $act = new LoginAct($medicoRepository);
+        return compact([ 'medicoRepository', 'act' ]);
+    }
+
+    /** @test */
+    public function deve_receber_exception_caso_o_medico_nao_exista() {
+        // Arrange
+        $sut = $this->getMockedSut();
+        $medico = Medico::factory()->make();
+        $dados = ['email' => $medico->email, 'senha' => '123456789'];
+
+        // Mock
+        $sut['medicoRepository']->shouldReceive('obter_medico_por_email')->once()->with($dados['email'])->andReturn(null);
+
+        // Assert
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Usuário \'' . $dados['email'] . '\' não encontrado.');
+
+        // Run
+        $sut['act']->executar($dados);
+    }
+
+    /** @test */
+    public function deve_receber_exception_caso_a_senha_nao_confere() {
+        // Arrange
+        $sut = $this->getMockedSut();
+        $medico = Medico::factory(['senha' => 'admin000'])->make();
+        $dados = ['email' => $medico->email, 'senha' => '000admin'];
+
+        // Mock
+        $sut['medicoRepository']->shouldReceive('obter_medico_por_email')->once()->with($dados['email'])->andReturn($medico);
+
+        // Assert
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Senha incorreta.');
+
+        // Run
+        $sut['act']->executar($dados);
+    }
+
+    /** @test */
+    public function deve_receber_exception_caso_o_medico_esteja_desativado() {
+        // Arrange
+        $sut = $this->getMockedSut();
+        $medico = Medico::factory(['senha' => 'admin000', 'status' => 'desativado'])->make();
+        $dados = ['email' => $medico->email, 'senha' => 'admin000'];
+
+        // Mock
+        $sut['medicoRepository']->shouldReceive('obter_medico_por_email')->once()->with($dados['email'])->andReturn($medico);
+
+        // Assert
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Você não possui mais acesso ao sistema. &#128533;');
+
+        // Run
+        $sut['act']->executar($dados);
+    }
+
+    /** @test */
+    public function deve_realizar_o_login() {
+        // Arrange
+        $sut = $this->getMockedSut();
+        $medico = Medico::factory(['senha' => 'admin000'])->make();
+        $dados = ['email' => $medico->email, 'senha' => 'admin000'];
+
+        // Assert
+        $sut['medicoRepository']->shouldReceive('obter_medico_por_email')->once()->with($dados['email'])->andReturn($medico);
+
+        // Run
+        $sut['act']->executar($dados);
+    }
+
+}
