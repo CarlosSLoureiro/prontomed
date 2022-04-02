7d8c13ea586a9ae05815fda357f267894b710633
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-02 15:13:28 -0300

    Factory (Paciente): define novo padrão de idade

--- database/factories/PacienteFactory.php
+++ database/factories/PacienteFactory.php
@@ -23,10 +23,10 @@ class PacienteFactory extends Factory
             'nome' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'telefone' => $this->faker->numerify('(##) #####-####'),
-            'nascimento' => $this->faker->dateTimeInInterval($startDate = '-18 years', $interval = '-32 years', $timezone = null)->format('Y-m-d'), //18-50
+            'nascimento' => $this->faker->dateTimeInInterval($startDate = '-18 years', $interval = '-22 years', $timezone = null)->format('Y-m-d'), //18-40
             'sexo' => $this->faker->randomElement(['masculino', 'feminino']),
             'tipo_sanguineo' => $this->faker->randomElement(['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-']),
-            'peso' => $this->faker->numberBetween(50, 90),
+            'peso' => $this->faker->numberBetween(50, 80),
             'altura' => ($this->faker->numberBetween(160, 180) / 100)
         ];
     }
