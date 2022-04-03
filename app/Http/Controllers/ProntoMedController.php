f06fa05bfdcbf31618a6856293920d6f2babb2e7
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-03 17:37:02 -0300

    Controller (ProntoMed): Nova definição para o front

--- app/Http/Controllers/ProntoMedController.php
+++ app/Http/Controllers/ProntoMedController.php
@@ -7,16 +7,9 @@ use App\Models\Paciente;
 class ProntoMedController extends SessionController
 {
     public function index() {
-        // Verifica se o médico ainda possui acesso ao sistema.
-        if (parent::medico()->status == 'ativo') {
-
-            return view('paginas.principal', [
-                'tipos_sanguineo' => Paciente::opcoes['tipo_sanguineo'],
-                'sexos' => Paciente::opcoes['sexo']
-            ]);
-
-        } else {
-            return parent::logout();
-        }
+        return view('prontomed', [
+            'tipos_sanguineo' => Paciente::opcoes['tipo_sanguineo'],
+            'sexos' => Paciente::opcoes['sexo']
+        ]);
     }
 }
