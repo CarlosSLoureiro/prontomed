894b3f29512537f8d0fb072cd554163f2a91427e
Carlos Loureiro <CARLIM.A7X@GMAIL.COM>
2022-04-03 17:32:20 -0300

    Reconfigura as rotas da aplicação
    Separando as rotas da api e web (para o front).

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\ConsultaController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['as' => 'api.'], function() {
+    Route::post('/login', [SessionController::class, 'logar'])->name('logar');
+    Route::get('/logout', [SessionController::class, 'logout'])->name('logout');
+
+    // Apenas medicos autenticados com JWT válido e médicos com status ativos
+    Route::group(['middleware' => ['auth:api', 'can:ativo']], function() {
+        // Cadastra nova senha
+        Route::get('/meus-dados', [SessionController::class, 'medico_dados'])->name('medico.dados');
+
+        // Cadastra nova senha
+        Route::post('/alterar-senha', [SessionController::class, 'alterar_senha'])->name('alterar.senha');
+
+        // Lista todos os pacientes
+        Route::get('/pacientes', [PacienteController::class, 'listar'])->name('pacientes.listar');
+
+        // Cadastra um novo paciente
+        Route::post('/paciente', [PacienteController::class, 'cadastrar'])->name('paciente.cadastrar');
+
+        // Edita um paciente
+        Route::put('/paciente/{id}', [PacienteController::class, 'editar'])->name('paciente.editar');
+
+        // Deleta um paciente
+        Route::delete('/paciente/{id}', [PacienteController::class, 'excluir'])->name('paciente.excluir');
+
+        // Lista consultas do médico de acordo com o tipo
+        Route::get('/consultas/{tipo}', [ConsultaController::class, 'listar'])->name('consultas.listar');
+
+        // Agenda uma nova consulta
+        Route::post('/consulta', [ConsultaController::class, 'agendar'])->name('consulta.agendar');
+
+        // Edita apenas a data agendada da consulta
+        Route::patch('/consulta/{id}', [ConsultaController::class, 'editar'])->name('consulta.editar');
+
+        // Deleta uma consulta
+        Route::delete('/consulta/{id}', [ConsultaController::class, 'excluir'])->name('consulta.excluir');
+
+        // Cadastra uma nova observação na consulta
+        Route::post('/consulta/{id}/observacao', [ConsultaController::class, 'cadastrar_observacao'])->name('consulta.cadastrar_observacao');
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProntoMedController;
-use App\Http\Controllers\SessionController;
-use App\Http\Controllers\PacienteController;
-use App\Http\Controllers\ConsultaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,58 +15,4 @@ use App\Http\Controllers\ConsultaController;
 |
 */
 
-// Apenas usuarios não autenticados
-Route::group(['middleware' => ['guest']], function() {
-
-    Route::group(['prefix' => 'api', 'as'=>'api.'], function(){
-        // Realiza o login
-        Route::post('/login', [SessionController::class, 'logar'])->name('logar');
-    });
-
-    // Exibe tela de login
-    Route::get('/login', [SessionController::class, 'login'])->name("login");
-});
-
-// Apenas usuário autenticados
-Route::group(['middleware' => ['auth']], function() {
-
-    // Exibe a tela principal
-    Route::get('/', [ProntoMedController::class, 'index'])->name("principal");
-
-    // Realiza o logout
-    Route::get('/logout', [SessionController::class, 'logout'])->name('logout');
-
-
-    // Apenas medicos ativos
-    Route::group(['middleware' => ['can:ativo'], 'prefix' => 'api', 'as' => 'api.'], function() {
-        // Cadastra nova senha
-        Route::post('/alterar-senha', [SessionController::class, 'alterar_senha'])->name('alterar.senha');
-
-        // Lista todos os pacientes
-        Route::get('/pacientes', [PacienteController::class, 'listar'])->name('pacientes.listar');
-
-        // Cadastra um novo paciente
-        Route::post('/paciente', [PacienteController::class, 'cadastrar'])->name('paciente.cadastrar');
-
-        // Edita um paciente
-        Route::put('/paciente/{id}', [PacienteController::class, 'editar'])->name('paciente.editar');
-
-        // Deleta um paciente
-        Route::delete('/paciente/{id}', [PacienteController::class, 'excluir'])->name('paciente.excluir');
-
-        // Lista consultas do médico de acordo com o tipo
-        Route::get('/consultas/{tipo}', [ConsultaController::class, 'listar'])->name('consultas.listar');
-
-        // Agenda uma nova consulta
-        Route::post('/consulta', [ConsultaController::class, 'agendar'])->name('consulta.agendar');
-
-        // Edita apenas a data agendada da consulta
-        Route::patch('/consulta/{id}', [ConsultaController::class, 'editar'])->name('consulta.editar');
-
-        // Deleta uma consulta
-        Route::delete('/consulta/{id}', [ConsultaController::class, 'excluir'])->name('consulta.excluir');
-
-        // Cadastra uma nova observação na consulta
-        Route::post('/consulta/{id}/observacao', [ConsultaController::class, 'cadastrar_observacao'])->name('consulta.cadastrar_observacao');
-    });
-});
+Route::get('/', [ProntoMedController::class, 'index'])->name("principal");
